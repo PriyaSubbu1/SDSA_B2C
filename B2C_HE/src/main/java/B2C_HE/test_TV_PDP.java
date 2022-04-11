d47de03f6553c0fa241893a00416ccc6ad875177
p.subburaj <p.subburaj@parner.samsung.com>
2022-04-11 12:26:38 -0400

    Code added
    javafile created and code added

---
+++ B2C_HE/src/main/java/B2C_HE/test_TV_PDP.java
@@ -0,0 +1,5 @@
+package B2C_HE;
+
+public class test_TV_PDP {
+
+}
---
+++ B2C_HE/src/test/java/B2C_HE/excecute_TV_PDP.java
@@ -0,0 +1,6 @@
+package B2C_HE;
+
+public class excecute_TV_PDP {
+	public static void main(String[] args) {
+	System.out.println("TV PDP code");
+}}
